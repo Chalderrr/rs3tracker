27395cace8a5d93f30f5237cd3b2c4276262719f
Liam Chalder <liam.chalder@laser.red>
2020-02-09 17:31:39 +0000

    make nicer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,10 @@
 
 
 @section('content')
-   
-    <div class="container">
-        <h1 class="title">Runescape 3 Adventurer's Log</h1>
-
-       
-    </div>
-
+    <section class="section">
+        <div class="container">
+            <h1 class="title">Runescape 3 Adventurer's Log</h1>
+            <h2 class="subtitle">Use the search below to return results for the given name</h2>
+        </div>
+    </section>
 @endsection
