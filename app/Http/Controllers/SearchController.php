2f3f40dee5e28fb4ebab7d53a82a577be85815ca
Liam Chalder <liam.chalder@laser.red>
2020-02-09 17:28:10 +0000

    set base method up

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use RuneStat\RS3\API;
+use Illuminate\Http\Request;
+
+
+class SearchController extends Controller {
+
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function search(Request $request)
+    {
+        
+    }
+
+}
