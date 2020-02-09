e76b2087ef8604d5f24290009ebb6277b1f244eb
Liam Chalder <liam.chalder@laser.red>
2020-02-09 18:59:34 +0000

    init api, find method in api to get profile, dd result

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,6 +16,18 @@ class SearchController extends Controller {
 
     public function search(Request $request)
     {
+        $this->validate($request, [
+            'search' => 'required|string'
+        ]);
+        $result = $request->input('search');
+
+        $rs3api = new API();
+
+        $player = $rs3api->getProfile($result);
+
+        dd($player);
+        // response in console > message: "Player [test] has their profile set to private"
+
         return response()->json([$request->all()]);
     }
 
