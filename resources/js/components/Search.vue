ab5282627b85acf3bb7139a1e4bd4693547b2944
Liam Chalder <liam.chalder@laser.red>
2020-02-09 18:43:54 +0000

    setup base component for search/post, display result @ output

---
+++ resources/js/components/Search.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <form @submit.prevent="submit">
+
+            <div class="field">
+                <label class="label">Search for a Player</label>
+                <div class="control">
+                    <input class="input" name="search" type="search" placeholder="Search..." v-model="search">
+                </div>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <button @click="submit" class="button is-link">Search</button>
+                </div>
+                <div class="control">
+                    <button class="button is-link is-light">Reset</button>
+                </div>
+            </div>     
+
+
+
+            <strong>Output:</strong>
+            <pre>{{result}}</pre>
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            search: '',
+            errors: null,
+            result: null,
+        }
+    },
+    methods: {
+        submit () {
+
+            axios.post('/search', {
+                search: this.search,
+            })
+            .then(response => {
+                this.result = response.data;
+            })
+            .catch(error => {   
+                this.errors = error.response.data.errors;
+            });
+        }
+    }
+
+}
+</script>
